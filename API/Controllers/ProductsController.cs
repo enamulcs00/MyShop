28e93c4693538e55597e2ddfbe9d0f2edca46649
Enamul Haque <enamulcs008@gmail.com>
2024-01-07 23:21:09 +0530

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,35 +1,41 @@
 using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{[ApiController]
+{
+    [ApiController]
 [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-      public ProductsController(StoreContext context)
+        private readonly IProductRepository _repo;
+      public ProductsController(IProductRepository repo)
       {
-            _context = context;
+           _repo = repo;
+            
         
       }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts() 
         {
-          return await _context.Products.ToListAsync();
+          return Ok(await _repo.GetProductsAsync());
         }
 
          [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProductById(int id) 
         {
-          return  await _context.Products.FindAsync(id);
+          return  await _repo.GetProductByIdAsync(id);
+        }
+         [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() 
+        {
+          return  Ok(await _repo.GetProductBrandsAsync());
         }
 
-            [HttpPost]
-        public string SaveData() 
+            [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() 
         {
-          return  "This will Save the data into DB";
+          return  Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
