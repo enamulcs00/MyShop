954c7afb665c302246905866d2f7092544802da9
Enamul Haque <enamulcs008@gmail.com>
2024-01-14 13:45:23 +0530

    End of section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,30 @@
+
+using SQLitePCL;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode , string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+         private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+          return  statusCode switch 
+          {
+            
+           400 => "A bad request you have made",
+           401 => "You are not authorize" ,
+           404 => "Resource not found",
+           500 => "Errors are the path to dark side. Errors leads to anger. Anger leads to hate. Hate leads to carreer change." ,
+           _=> null
+          };
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
-using Core.Interfaces;
+using API.Extensions;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,24 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddDbContext<StoreContext>(store=>{
-    store.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository , ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
